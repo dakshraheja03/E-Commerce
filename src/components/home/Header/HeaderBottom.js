e9bf4bf58bbb686621cd014a8020d0a116173482
Daksh Raheja <dakshraheja03@gmail.com>
2024-02-27 05:03:59 +0530

    id

--- src/components/home/Header/HeaderBottom.js
+++ src/components/home/Header/HeaderBottom.js
@@ -146,7 +146,7 @@ const HeaderBottom = () => {
             )}
           </div>
           <div className="flex gap-4 mt-2 lg:mt-0 items-center pr-6 cursor-pointer relative">
-            <div onClick={() => setShowUser(!showUser)} className="flex">
+            <div onClick={() => setShowUser(!showUser)} className="flex" id="userdropdown">
               <FaUser />
               <FaCaretDown />
             </div>
@@ -158,7 +158,7 @@ const HeaderBottom = () => {
                 className="absolute top-6 left-0 z-50 bg-primeColor w-44 text-[#767676] h-auto p-4 pb-6"
               >
                 <Link to="/signin">
-                  <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer" onClick={handle}>
+                  <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer" onClick={handle} id="logout">
                     {token!==null?(<p>Logout</p>):(<p>Login</p>)}
                   </li>
                 </Link>
